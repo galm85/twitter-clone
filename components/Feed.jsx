aecf6becaedfe3f532ebbcb5405e1117707a9269
Gal Mizrahi <galmizrahi@Gals-iMac.lan>
2022-08-11 13:45:40 +0300

    add Post section in feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,30 @@
 import { SparklesIcon } from "@heroicons/react/outline"
 import Input from "./Input"
+import Post from "./Post"
 
 const Feed = () => {
+
+  const posts = [
+    {
+      id:"1",
+      name:'Gal Mizrahi',
+      username:'galmizrahi',
+      userImg:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80',
+      img:"https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2948&q=80",
+      text:"nice view",
+      timestamp:"2 hours ago"
+    },
+    {
+      id:"2",
+      name:'Gal Mizrahi',
+      username:'galmizrahi',
+      userImg:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80',
+      img:"https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+      text:"sunset",
+      timestamp:"3 days ago"
+    },
+  ]
+
   
     return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
@@ -14,7 +37,9 @@ const Feed = () => {
       </div>
 
       <Input/>
-    
+    {posts.map((post)=>(
+      <Post key={post.id} post={post}/>
+    ))}
     </div>
   )
 
---
+++ components/Post.jsx
@@ -0,0 +1,47 @@
+import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from "@heroicons/react/outline"
+
+const Post = ({post}) => {
+  
+  
+    return (
+    <div className="flex p-3 cursor-pointer border-b border-gray-200">
+        {/*user image */}
+        <img src={post.userImg} alt="user image" className="h-11 w-11 rounded-full mr-4" />
+
+
+        {/* right side */}
+        <div>
+            {/* header */}
+            <div className="flex items-center justify-between">
+                {/* host user info */}
+                <div className="flex space-x-1 whitespace-nowrap items-center">
+                    <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.name}</h4>
+                    <span className="text-sm sm:text-[15px]">@{post.username} - </span>
+                    <span className="text-sm sm:text-[15px] hover:underline">{post.timestamp}</span>
+                </div>
+                {/* dots icon */}
+                <DotsHorizontalIcon className=" hoverEffect h-10 w-10 hover:bg-sky-100 hover:text-sky-500 p-2"/>
+            </div>
+
+            {/* post text */}
+            <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">{post.text}</p>
+
+            {/* post image */}
+            <img src={post.img} alt="post image" className="rounded-2xl mr-2" />
+
+            {/* actions icons */}
+            <div className="flex justify-between text-gray-500 p-2">
+                <ChatIcon className="w-9 h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
+                <TrashIcon className="w-9 h-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"/>
+                <HeartIcon className="w-9 h-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"/>
+                <ShareIcon className="w-9 h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
+                <ChartBarIcon className="w-9 h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
+            </div>
+
+        </div>
+    </div>
+  )
+
+}
+
+export default Post
